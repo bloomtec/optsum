ef5b173527f030d8619547ddcf7ac89e2691ead0
Julio César Domínguez Giraldo <jucedogi@ubuntu.ubuntu-domain>
2012-09-13 11:21:53 -0500

    revs

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,18 +7,18 @@ App::uses('AppModel', 'Model');
  */
 class Product extends AppModel {
 
-/**
- * Display field
- *
- * @var string
- */
+	/**
+	 * Display field
+	 *
+	 * @var string
+	 */
 	public $displayField = 'category_id';
 
-/**
- * Validation rules
- *
- * @var array
- */
+	/**
+	 * Validation rules
+	 *
+	 * @var array
+	 */
 	public $validate = array(
 		'category_id' => array(
 			'numeric' => array(
@@ -34,11 +34,11 @@ class Product extends AppModel {
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * belongsTo associations
- *
- * @var array
- */
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
 	public $belongsTo = array(
 		'Category' => array(
 			'className' => 'Category',
@@ -48,4 +48,26 @@ class Product extends AppModel {
 			'order' => ''
 		)
 	);
+	
+	/**
+	 * hasOne associations
+	 *
+	 * @var array
+	 */
+	public $hasOne = array(
+		'Image' => array(
+			'className' => 'Image',
+			'foreignKey' => 'foreign_key',
+			'dependent' => true,
+			'conditions' => array('Image.model_class' => 'Product'),
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+	
 }
